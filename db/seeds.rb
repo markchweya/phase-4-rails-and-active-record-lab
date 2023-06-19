1f9337a39cb910dae6424d52ac74578e630fe7ac
Mark Chweya <chweyamark@gmail.com>
2023-06-19 23:59:18 +0300

    Completed Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.destroy_all
+
+puts "🌱 Seeding spices..."
+
+puts '🌱 Seeding spices..."'
+10.times do
+  Student.create(first_name: Faker::Name.name, last_name: Faker::Name.name)
+  Student.create(first_name: Faker::Name.name, last_name: Faker::Name.name, grade: rand(40..99))
+end
+
+puts "✅ Done seeding!"
+puts '✅ Done seeding!'
+rand(40, ..99)
